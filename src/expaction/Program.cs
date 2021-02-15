82fb0ce6c3949bbccea8e3b40742d89b5d4de9dd
Brian Randell (Outlook) <brian.randell@outlook.com>
2021-02-15 11:39:38 -0800

    Adjust reading variables and only root directory enum

--- src/expaction/Program.cs
+++ src/expaction/Program.cs
@@ -7,16 +7,25 @@ namespace expaction
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
             foreach (string arg in args)
             {
-                Console.WriteLine(arg);
+                if (arg.Contains("not"))
+                {
+                    Console.WriteLine("Found it!");
+                }
+                else
+                {
+                    Console.WriteLine(arg);
+                }
             }
-            Console.WriteLine("::set-output name=message::Hello, Github Actions");
 
-            foreach (string file in System.IO.Directory.EnumerateFiles("/github/workspace", "*", System.IO.SearchOption.AllDirectories))
+            foreach (string file in System.IO.Directory.EnumerateFiles("/github/workspace", "*"))
             {
                 Console.WriteLine(file);
             }
+
+            Console.WriteLine("::set-output name=message::Hello, Github Actions");
         }
     }
 }
